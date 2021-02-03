0fe6896cf33a5720a4666ab44894304cb76cb733
saurabhpuri <SaurabhPuri2265>
2021-02-03 17:02:41 +0530

    Contact Manipulation service added

--- src/main/java/com/contactList/manager/controllers/EditController.java
+++ src/main/java/com/contactList/manager/controllers/EditController.java
@@ -2,10 +2,14 @@ package com.contactList.manager.controllers;
 
 import com.contactList.manager.models.Contact;
 import com.contactList.manager.repositories.ContactListRepository;
+import com.contactList.manager.services.ManipulationService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping({"/contacts","/contacts/"})
@@ -13,6 +17,9 @@ public class EditController {
 
     @Autowired
     private ContactListRepository contactListRepository;
+
+    @Autowired
+    private ManipulationService manipulationService;
 
     //Create a new contact inside the contactlist repo
     @PostMapping
@@ -23,18 +30,30 @@ public class EditController {
 
     //Update a contact inside the contactlist repo
     @RequestMapping(value = "{id}",method = RequestMethod.PUT)
-    public void updateContact(@PathVariable Integer id,@RequestBody Contact contact)
+    public void update(@PathVariable Integer id,@RequestBody Contact contact)
     {
-        Contact existingcontact = contactListRepository.getOne(id);
-        BeanUtils.copyProperties(contact,existingcontact,"id");
-        contactListRepository.saveAndFlush(existingcontact);
+        try {
+            manipulationService.updateService(id,contact);
+        }
+        catch (RuntimeException e)
+        {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,e.getMessage());
+        }
+
     }
 
     //Delete a contact inside the contactlist repo
     @RequestMapping(value = "{id}",method = RequestMethod.DELETE)
     public void delete(@PathVariable Integer id)
     {
-        contactListRepository.deleteById(id);
+        try{
+            manipulationService.deleteService(id);
+        }
+        catch (RuntimeException e)
+        {
+            throw new NoSuchElementException(e.getMessage());
+        }
     }
 
 }
+
---
+++ src/main/java/com/contactList/manager/services/ManipulationService.java
@@ -0,0 +1,44 @@
+package com.contactList.manager.services;
+
+import com.contactList.manager.models.Contact;
+import com.contactList.manager.repositories.ContactListRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.sound.midi.Soundbank;
+import java.util.NoSuchElementException;
+
+@Service
+public class ManipulationService {
+
+    @Autowired
+    ContactListRepository contactListRepository;
+
+    //UpdateService to update contact details
+    public void updateService(Integer id, Contact contact) throws RuntimeException
+    {
+            if(contactListRepository.existsById(id))
+            {
+                Contact existingcontact = contactListRepository.getOne(id);
+                System.out.println(existingcontact);
+                BeanUtils.copyProperties(contact,existingcontact,"serial");
+                contactListRepository.saveAndFlush(existingcontact);
+            }
+            else
+                throw new NoSuchElementException("Requested contact does not exist");
+    }
+
+    //DeleteService to delete contact
+    public void deleteService(Integer id) throws RuntimeException
+    {
+        if(contactListRepository.existsById(id))
+        {
+            contactListRepository.deleteById(id);
+        }
+        else
+            throw new NoSuchElementException("Contact already not present");
+    }
+
+}
