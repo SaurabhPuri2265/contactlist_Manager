256f66248ca33eada27e30e3c906b2ef8c455f17
saurabhpuri <SaurabhPuri2265>
2021-02-03 22:48:48 +0530

    Created Order Model

--- src/main/java/com/contactList/manager/models/Order.java
+++ src/main/java/com/contactList/manager/models/Order.java
@@ -1,19 +1,26 @@
 package com.contactList.manager.models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity(name = "orders")
+@Getter @Setter @NoArgsConstructor
 public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int order_id;
 
-
     private String order_item;
     private String order_status;
-    private String customer_id;
+
+    private int customer_id;
+
 }
