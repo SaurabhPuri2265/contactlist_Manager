37ec9a86a9eed7af118c886e2b8a0d62823a4639
saurabhpuri <SaurabhPuri2265>
2021-02-05 22:44:45 +0530

    Purchase Service Test

--- src/test/java/com/contactList/manager/services/PurchaseServiceTest.java
+++ src/test/java/com/contactList/manager/services/PurchaseServiceTest.java
@@ -4,28 +4,23 @@ import com.contactList.manager.models.Contact;
 import com.contactList.manager.models.Order;
 import com.contactList.manager.repositories.ContactRepository;
 import com.contactList.manager.repositories.OrderRepository;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doReturn;
+
 
 
 @RunWith(MockitoJUnitRunner.class)
-class PurchaseServiceTest {
+public class PurchaseServiceTest {
 
     @InjectMocks
     PurchaseService purchaseService;
@@ -37,18 +32,8 @@ class PurchaseServiceTest {
     ContactRepository contactRepository;
 
 
-    @BeforeClass
-    void setUp() {
-        System.out.println("Service layer testing started ->");
-    }
-
-    @AfterClass
-    void tearDown() {
-        System.out.println("Service layer testing ended ->");
-    }
-
     @Test
-    void customerOrders() throws NullPointerException{
+    public void customerOrders() throws NullPointerException{
 
         Contact contact=new Contact();
         contact.setSerial(1);
@@ -62,8 +47,7 @@ class PurchaseServiceTest {
         order.setOrder_status("shipped");
         order.setContact(contact);
 
-        //contactRepository.save(contact);
-        orderRepository.save(order);
+        System.out.println(order);
 
         Object[] arr=new Object[]{100,"mobile","shipped"};
 
@@ -71,17 +55,33 @@ class PurchaseServiceTest {
 
         orderList.add(arr);
 
+        doReturn(orderList).when(orderRepository).findByContact(contact.getSerial());
+
         System.out.println(orderList);
 
-        when(orderRepository.findByContact(contact.getSerial())).thenReturn(orderList);
-
-        assertEquals(orderList,purchaseService.customerOrders(contact.getSerial()));
-
+        //Execute service call
+        assertEquals(orderList.size(), purchaseService.customerOrders(contact.getSerial()));
+        assertThrows(RuntimeException.class,() -> purchaseService.customerOrders(10),"Nothing found");
 
 
     }
 
     @Test
-    void getStatus() {
+    public void getStatus() {
+        Contact contact=new Contact();
+        contact.setSerial(1);
+        contact.setContact_name("Saurabh");
+        contact.setContact_number("123456");
+
+
+        Order order= new Order();
+        order.setOrder_id(100);
+        order.setOrder_item("mobile");
+        order.setOrder_status("shipped");
+        order.setContact(contact);
+
+        doReturn(order.getOrder_status()).when(orderRepository).getOrderStatus(1,"mobile");
+
+        assertEquals("shipped",purchaseService.getStatus(contact.getSerial(),"mobile"));
     }
 }
